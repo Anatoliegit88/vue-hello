07de791a9b0ec618586a9856b620c1d2b3c3b10b
Anatoliegit88 <anatolie.coroban88@gmail.com>
2022-10-25 17:11:25 +0200

    Vue

---
+++ script.js
@@ -0,0 +1,10 @@
+const { createApp } = Vue;
+
+createApp({
+  data() {
+    return {
+      message: "Un cuore grande si riempie con poco!!!",
+      Image: "pexels-pixabay-210012.jpg",
+    };
+  },
+}).mount("#app");
